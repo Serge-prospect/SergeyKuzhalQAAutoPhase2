043ed89f59e03ac911a712a2bb0a83fc177b0f77
Serge-prospect <prospect@tut.by>
2022-08-28 23:47:36 +0300

    Task50.8.  - -  Add RefreshPageAt50 auto script.

---
+++ Task50/Task50_8/RefreshPageAt50/RefreshPageAt50/RefreshPageAt50/BootstrapDownloadProgressDemoPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Chrome;
+
+namespace RefreshPageAt50
+{
+    public class BootstrapDownloadProgressDemoPage
+    {
+        const string URL = "https://demo.seleniumeasy.com/bootstrap-download-progress-demo.html";
+        public By DownloadButton = By.Id("cricle-btn");
+        public By PctProgress = By.CssSelector("#fakhar-cricle .percenttext");
+        IWebDriver _driver;
+        WebDriverWait _wait;
+
+        public BootstrapDownloadProgressDemoPage(IWebDriver driver, int[] timeOutAndPollTime)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(_driver, timeout: TimeSpan.FromSeconds(timeOutAndPollTime[0]))
+            {
+                PollingInterval = TimeSpan.FromSeconds(timeOutAndPollTime[1])
+            };
+        }
+
+        public static BootstrapDownloadProgressDemoPage GoToBootstrapDownloadProgressDemoPage(IWebDriver driver, int[] timeOutAndPollTime)
+        {
+            driver.Navigate().GoToUrl(URL);
+            return new BootstrapDownloadProgressDemoPage(driver, timeOutAndPollTime);
+        }
+
+        public IWebElement GetelementBy(By elementBy)
+        {
+            try
+            {
+                _wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+                return _wait.Until(ExpectedConditions.ElementExists(elementBy));
+            }
+            catch { return null; }
+        }
+
+        public bool IsProgressAchievedPctAmount(int pctAmount)
+        {
+            try
+            {
+                return _wait.Until(pct => Convert.ToInt32(GetelementBy(PctProgress)?.Text.TrimEnd('%')) >= pctAmount);                
+            }
+            catch { return false; }
+        }
+
+        public void RefreshPage()
+        {
+            Console.WriteLine("The page is refreshed.");
+            _driver.Navigate().Refresh();
+        }
+    }
+}
---
+++ Task50/Task50_8/RefreshPageAt50/RefreshPageAt50/RefreshPageAt50/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace RefreshPageAt50
+{
+    class Program
+    {
+        const int PCT_AMOUNT = 50;
+        static readonly int[] _timeOutAndPollTime = new int[2] { 60, 2 };
+
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver(".");
+            var page = BootstrapDownloadProgressDemoPage.GoToBootstrapDownloadProgressDemoPage(driver, _timeOutAndPollTime);
+            
+            page.GetelementBy(page.DownloadButton)?.Click();
+
+            if (page.IsProgressAchievedPctAmount(PCT_AMOUNT))
+                page.RefreshPage();
+            
+            driver.Quit();
+        }
+    }
+}
