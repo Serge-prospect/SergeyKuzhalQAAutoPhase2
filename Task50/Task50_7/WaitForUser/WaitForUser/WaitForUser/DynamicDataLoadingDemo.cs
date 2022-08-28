a3ebc0f3e5e9f08ba0698541bd1ce7da502a822e
Serge-prospect <prospect@tut.by>
2022-08-28 20:23:29 +0300

    Task50.7  - -  Add WaitForUser auto script.

---
+++ Task50/Task50_7/WaitForUser/WaitForUser/WaitForUser/DynamicDataLoadingDemo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace WaitForUser
+{
+    public class DynamicDataLoadingDemo
+    {
+        const string URL = "https://demo.seleniumeasy.com/dynamic-data-loading-demo.html";
+        public readonly By GetNewUserButton = By.Id("save");
+        public readonly By newUserPhotoLocator = By.CssSelector("#loading>img[src*='api/portraits'][src*='.jpg']");
+        public readonly By newUserFullNameLocator =
+                            By.XPath("//div[contains(text()[1],'First Name') and contains(text()[2],'Last Name')]");
+        readonly IWebDriver _driver;
+        readonly WebDriverWait _wait;
+
+        public DynamicDataLoadingDemo(IWebDriver driver, int waitTime)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(waitTime));
+        }
+        public static DynamicDataLoadingDemo GoToDynamicDataLoadingDemoPage(IWebDriver driver, int waitTime)
+        {
+            driver.Navigate().GoToUrl(URL);
+            return new DynamicDataLoadingDemo(driver, waitTime);
+        }
+
+        public IWebElement GetelementBy(By elementBy)
+        {
+            try
+            {
+                return _wait.Until(ExpectedConditions.ElementExists(elementBy));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public bool IsNewUserDisplayed()
+        {
+            try
+            {
+                return GetelementBy(newUserPhotoLocator).Displayed
+                    && GetelementBy(newUserFullNameLocator).Displayed;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ Task50/Task50_7/WaitForUser/WaitForUser/WaitForUser/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WaitForUser
+{
+    class Program
+    {
+        static readonly int _waitTime = 30;
+        static readonly string[] _newUserStatus = new string[2] { "New user is not displayed.", "New user is displayed." };
+
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(".");
+            DynamicDataLoadingDemo page = DynamicDataLoadingDemo.GoToDynamicDataLoadingDemoPage(driver, _waitTime);
+
+            try
+            {
+                page.GetelementBy(page.GetNewUserButton)?.Click();
+
+                bool isNewUserDisplayed = page.IsNewUserDisplayed();
+                PrintNewUserStatus(isNewUserDisplayed);
+            }
+            finally { driver.Quit(); }
+        }
+
+        static void PrintNewUserStatus(bool isNewUserDisplayed)
+        {
+            if (isNewUserDisplayed)
+                Console.WriteLine(_newUserStatus[1]);
+            else
+                Console.WriteLine(_newUserStatus[0]);
+        }
+    }
+}
